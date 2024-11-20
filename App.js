e0b603033fbc5c6064c46217106b17a5d4d409bc
aftab0045 <aftabattar0045@gmail.com>
2024-11-20 23:27:15 +0530

    adss

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-const heading = React.createElement("div",{id:"heading"},
-    [React.createElement("div",{},
-        [React.createElement("h1",{},"Hello Aftab From H1 !"),
-        React.createElement("h1",{},"Hello Aftab From H2!")])],
-        
-    React.createElement("div",{},
-        [React.createElement("h1",{},"Hello Aftab From H3 !"),
-        React.createElement("h1",{},"Hello Aftab From H4!")]));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// React Element
+const heading = React.createElement("h1", { id: "heading" }, "Hello Aftab! From CreateElement");
+
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS => JS Object => HTMLElement Render
+const jsxHeading = <h1 id='heading'>Hello Aftab From JSX</h1>;
+
+
+
+// React Functional Component
+const Title = () => (
+    <h1 className='head'>Hello From Functional Component2!</h1>
+);
+
+const HeadingComponent = () => (
+    <div>
+        {/* {Title()}
+        <Title></Title> */}         
+        <Title />
+        <h1 id='heading'>Hello From Functional Component!</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
-
-
-
-
-
-
+root.render(<HeadingComponent />);
