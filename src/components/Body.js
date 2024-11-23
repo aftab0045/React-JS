7b91c54db0596f5712a0d999342f40c0100b466e
aftab0045 <aftabattar0045@gmail.com>
2024-11-24 00:30:09 +0530

    add

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockdata";
-import { useState } from "react";
-
+import { useState , useEffect } from "react";
+import shimmer from "./shimmer";
 
 
 
 
 const Body = () =>{
     //Local State Variable - Super powerfull Variable
-    const [listOfRestaurants , setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants , setListOfRestaurants] = useState([]);
 
 
+    useEffect(() =>{
+      fetchData();
+    },[]);
+
+    const fetchData = async() =>{
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5625986&lng=73.8523582&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const json = await data.json();
+
+    console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    }
+
+    if (listOfRestaurants.length == 0){
+      return <shimmer></shimmer>;
+    }
+   
 
     return(
       <div className='body'>
